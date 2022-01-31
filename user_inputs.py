21e91b7962d1f1873991eb8c0ed39334a8d5af36
skandula9273 <50324546+skandula9273@users.noreply.github.com>
2022-01-31 17:13:15 -0500

    Delete user_inputs.py

--- user_inputs.py
+++
@@ -1,5 +0,0 @@
-attraction_type = input("What type of attraction (Sports and Adventure, Museums and Historic Sites, Eat and Drink, Nightlife and Entertainment) do you want? ").lower()
-entry = input("Does the attraction have to be free or it can be paid? ").lower()
-timing = input("Do you want the attraction to be open, day(until 9-10 PM) or extended(until Midnight)? ").lower()
-age_restriction = input("Do you want any age restrictions, no (None) or only adults (Adults)? ").lower()
-acommodation = input("Do you want outdoor or indoor? ").lower()
